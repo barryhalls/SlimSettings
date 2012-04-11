8c1c6fc3bf6cc487f6a38b749cdf08434e0707ee
ar <krarvind@gmail.com>
2012-04-10 23:13:51 -0300

    Status bar transperancy and changed Wi-Fi AP text to AP

--- src/com/ar/slimsettings/fragments/StatusBarGeneral.java
+++ src/com/ar/slimsettings/fragments/StatusBarGeneral.java
@@ -4,6 +4,7 @@ package com.ar.slimsettings.fragments;
 import android.content.Context;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
@@ -14,17 +15,22 @@ import com.ar.slimsettings.R;
 import com.ar.slimsettings.SettingsPreferenceFragment;
 import com.ar.slimsettings.util.Helpers;
 
-public class StatusBarGeneral extends SettingsPreferenceFragment {
+public class StatusBarGeneral extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener  {
 
     private static final String PREF_SETTINGS_BUTTON_BEHAVIOR = "settings_behavior";
     private static final String PREF_AUTO_HIDE_TOGGLES = "auto_hide_toggles";
     private static final String PREF_BRIGHTNESS_TOGGLE = "status_bar_brightness_toggle";
     private static final String PREF_ADB_ICON = "adb_icon";
+    private static final String PREF_TRANSPARENCY = "status_bar_transparency";
+    private static final String PREF_LAYOUT = "status_bar_layout";
 
     CheckBoxPreference mDefaultSettingsButtonBehavior;
     CheckBoxPreference mAutoHideToggles;
     CheckBoxPreference mStatusBarBrightnessToggle;
     CheckBoxPreference mAdbIcon;
+    ListPreference mTransparency;
+    ListPreference mLayout;
 
     Context mContext;
 
@@ -55,14 +61,28 @@ public class StatusBarGeneral extends SettingsPreferenceFragment {
         mAdbIcon = (CheckBoxPreference) findPreference(PREF_ADB_ICON);
         mAdbIcon.setChecked(Settings.Secure.getInt(getActivity().getContentResolver(),
                 Settings.Secure.ADB_ICON, 1) == 1);
+        
+        mTransparency = (ListPreference) findPreference(PREF_TRANSPARENCY);
+        mTransparency.setOnPreferenceChangeListener(this);
+        mTransparency.setValue(Integer.toString(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.STATUS_BAR_TRANSPARENCY,
+                100)));
+
+        mLayout = (ListPreference) findPreference(PREF_LAYOUT);
+        mLayout.setOnPreferenceChangeListener(this);
+        mLayout.setValue(Integer.toString(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.STATUS_BAR_LAYOUT,
+                0)));
+
 
         if (mTablet) {
             PreferenceScreen prefs = getPreferenceScreen();
             prefs.removePreference(mStatusBarBrightnessToggle);
             prefs.removePreference(mAutoHideToggles);
             prefs.removePreference(mDefaultSettingsButtonBehavior);
+            prefs.removePreference(mTransparency);
+            prefs.removePreference(mLayout);
         }
-
     }
 
     @Override
@@ -98,10 +118,27 @@ public class StatusBarGeneral extends SettingsPreferenceFragment {
             Settings.Secure.putInt(getActivity().getContentResolver(),
                     Settings.Secure.ADB_ICON, checked ? 1 : 0);
             return true;
+            
         }
 
         return super.onPreferenceTreeClick(preferenceScreen, preference);
 
     }
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean result = false;
 
+        if (preference == mTransparency) {
+            int val = Integer.parseInt((String) newValue);
+            result = Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUS_BAR_TRANSPARENCY, val);
+            Helpers.restartSystemUI();
+        } else if (preference == mLayout) {
+            int val = Integer.parseInt((String) newValue);
+            result = Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUS_BAR_LAYOUT, val);
+            Helpers.restartSystemUI();
+        }
+        return result;
+    }
 }
